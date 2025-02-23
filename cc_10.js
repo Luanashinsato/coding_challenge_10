91c64a5052448ea3e1630d4cf1131312d5ea3e86
Luanashinsato <lshinsato@usf.edu>
2025-02-23 18:38:32 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -22,3 +22,24 @@ console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, 
 
 prod1.updateStock(3);
 console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+
+// Task 2: Creating an Order Class 
+class Order {
+    // Creating Order class
+    constructor(orderId, product, quantity) { 
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.product.updateStock(this.quantity); // Ensures stock is reduced when an order is created
+    }
+    // Adding a method that returns order details
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}"`;
+    }
+}
+// Test Cases
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); // Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
+
